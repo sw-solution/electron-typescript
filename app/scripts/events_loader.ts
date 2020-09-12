5d2062b3c47d39e50c547991235587311ad3bab2
superdev0505 <grantlatest@gmail.com>
2020-09-12 09:38:32 +0200

    - revert the setting token null.

--- app/scripts/events_loader.ts
+++ app/scripts/events_loader.ts
@@ -48,11 +48,6 @@ axios.interceptors.response.use(
   }
 );
 
-if (process.env.NODE_ENV === 'production') {
-  tokenStore.set('mtp', null);
-  tokenStore.set('mapillary', null);
-}
-
 export default (mainWindow: BrowserWindow, app: App) => {
   const basepath = app.getAppPath();
 
